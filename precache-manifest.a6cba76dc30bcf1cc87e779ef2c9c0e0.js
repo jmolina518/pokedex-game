b55f66b055c44a64d87c857cccbf62eea48ba895
jmolina518 <jmolina518@gmail.com>
2020-07-31 12:00:17 -0500

    Updates

---
+++ precache-manifest.a6cba76dc30bcf1cc87e779ef2c9c0e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a78d09651e553e06a86a9c82052d459",
+    "url": "/pokedex-game/index.html"
+  },
+  {
+    "revision": "eeff5ded49de3e83fdf4",
+    "url": "/pokedex-game/static/css/main.848dbf14.chunk.css"
+  },
+  {
+    "revision": "6ffe179f5a73b66f0fa8",
+    "url": "/pokedex-game/static/js/2.9bff95fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex-game/static/js/2.9bff95fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeff5ded49de3e83fdf4",
+    "url": "/pokedex-game/static/js/main.fb9699b7.chunk.js"
+  },
+  {
+    "revision": "87822e887cc42bb0afd0",
+    "url": "/pokedex-game/static/js/runtime-main.48be723b.js"
+  }
+]);
